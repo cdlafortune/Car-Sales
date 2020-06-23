35c94c29e9c54a90a060347a513a8c21642d26e9
cdsuddreth <cdsuddreth@gmail.com>
2020-06-22 21:39:34 -0400

    added redux for state management, working on adding features

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import {Provider} from "react-redux";
 import 'bulma/css/bulma.css';
 import './styles.scss';
+import {store} from "./state/reducer";
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Provider store={store}><App /></Provider>, rootElement);
